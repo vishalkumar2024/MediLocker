024d869f0f9219201b97b6dc6e50835b5dad2014
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-05-20 12:41:33 +0530

    add logo to RegisterPage component and update styling for input fields

--- Frontend/src/Pages/Register.jsx
+++ Frontend/src/Pages/Register.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { Heart, Eye, EyeOff, Loader2 } from 'lucide-react'
+import logo from '../assets/Logo.jpg'
 
 const bloodGroups = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']
 
 function RegisterPage() {
+
   const navigate = useNavigate()
   const [step, setStep] = useState(1)
   const [showPass, setShowPass] = useState(false)
@@ -38,8 +40,8 @@ function RegisterPage() {
       <div className="relative z-10 w-full max-w-md">
         <div className="text-center mb-4 ">
           <Link to="/" className="inline-flex items-center gap-2.5 mb-2">
-            <div className="w-9 h-9 bg-green-500 rounded-md flex items-center justify-center">
-              <Heart className="w-5 h-5 text-white" />
+            <div className="w-9 h-9 bg-white rounded-lg flex items-center justify-center">
+              <img src={logo} alt="logo" className="w-[90%] h-[90%] " />
             </div>
             <span className="font-logo text-xl font-bold text-white">MediLocker</span>
           </Link>
@@ -47,7 +49,7 @@ function RegisterPage() {
           <p className="text-white/40 text-sm">Step {step} of 2 — {step === 1 ? 'Account Info' : 'Health Profile'}</p>
         </div>
 
-        {/* Progress */}
+        {/* Step{1,2} color */}
         <div className="flex gap-2 mb-6">
           <div className="flex-1 h-1 rounded-full bg-cyan-500" />
           <div className={`flex-1 h-1 rounded-full transition-all ${step === 2 ? 'bg-cyan-500' : 'bg-white/10'}`} />
@@ -65,7 +67,7 @@ function RegisterPage() {
                   type="text"
                   value={form.name}
                   onChange={e => update('name', e.target.value)} placeholder="Arjun Sharma"
-                  className="w-full bg-surface-800/80 border border-white/10 focus:border-cyan-500/60 text-white placeholder:text-white/30 rounded-xl px-4 py-3 outline-none transition-all duration-200 focus:ring-2 focus:ring-cyan-500/20" required
+                  className="w-full bg-[#1b2335] border border-white/10 focus:border-cyan-500/60 text-white placeholder:text-white/30 rounded-xl px-4 py-3 outline-none transition-all duration-200 focus:ring-2 focus:ring-cyan-500/20" required
                 />
               </div>
               <div>
@@ -74,7 +76,7 @@ function RegisterPage() {
                   type="email"
                   value={form.email}
                   onChange={e => update('email', e.target.value)} placeholder="you@example.com"
-                  className="w-full bg-surface-800/80 border border-white/10 focus:border-cyan-500/60 text-white placeholder:text-white/30 rounded-xl px-4 py-3 outline-none transition-all duration-200 focus:ring-2 focus:ring-cyan-500/20" required
+                  className="w-full bg-[#1b2335] border border-white/10 focus:border-cyan-500/60 text-white placeholder:text-white/30 rounded-xl px-4 py-3 outline-none transition-all duration-200 focus:ring-2 focus:ring-cyan-500/20" required
                 />
               </div>
               <div>
@@ -83,7 +85,7 @@ function RegisterPage() {
                   type="tel"
                   value={form.phone}
                   onChange={e => update('phone', e.target.value)} placeholder="+91 98765 43210"
-                  className="w-full bg-surface-800/80 border border-white/10 focus:border-cyan-500/60 text-white placeholder:text-white/30 rounded-xl px-4 py-3 outline-none transition-all duration-200 focus:ring-2 focus:ring-cyan-500/20"
+                  className="w-full bg-[#1b2335] border border-white/10 focus:border-cyan-500/60 text-white placeholder:text-white/30 rounded-xl px-4 py-3 outline-none transition-all duration-200 focus:ring-2 focus:ring-cyan-500/20"
                 />
               </div>
               <div>
@@ -94,10 +96,13 @@ function RegisterPage() {
                     value={form.password}
                     onChange={e => update('password', e.target.value)}
                     placeholder="Create a strong password"
-                    className="w-full bg-surface-800/80 border border-white/10 focus:border-cyan-500/60 text-white placeholder:text-white/30 rounded-xl px-4 py-3 outline-none transition-all duration-200 focus:ring-2 focus:ring-cyan-500/20 pr-11"
+                    className="w-full bg-[#1b2335] border border-white/10 focus:border-cyan-500/60 text-white placeholder:text-white/30 rounded-xl px-4 py-3 outline-none transition-all duration-200 focus:ring-2 focus:ring-cyan-500/20 pr-11"
                     required minLength={6}
                   />
-                  <button type="button" onClick={() => setShowPass(p => !p)} className="absolute right-3.5 top-1/2 -translate-y-1/2 text-white/30 hover:text-white/60">
+                  <button
+                    type="button"
+                    onClick={() => setShowPass(p => !p)} className="absolute right-3.5 top-1/2 -translate-y-1/2 text-white/30 hover:text-white/60"
+                  >
                     {showPass ? <EyeOff className="w-4 h-4" /> : <Eye className="w-4 h-4" />}
                   </button>
                 </div>
@@ -113,7 +118,7 @@ function RegisterPage() {
                 <input
                   type="date"
                   value={form.dob}
-                  onChange={e => update('dob', e.target.value)} className="w-full bg-surface-800/80 border border-white/10 focus:border-cyan-500/60 text-white placeholder:text-white/30 rounded-xl px-4 py-3 outline-none transition-all duration-200 focus:ring-2 focus:ring-cyan-500/20"
+                  onChange={e => update('dob', e.target.value)} className="w-full bg-[#1b2335] border border-white/10 focus:border-cyan-500/60 text-white placeholder:text-white/30 rounded-xl px-4 py-3 outline-none transition-all duration-200 focus:ring-2 focus:ring-cyan-500/20"
                   required
                 />
               </div>
